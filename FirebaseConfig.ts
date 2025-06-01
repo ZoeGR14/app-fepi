7ce2d69f73af17465af8d7b2014ea12fdf590e4e
ZoeGR14 <zgonzalezr2001@alumno.ipn.mx>
2025-06-01 01:31:56 -0600

    Integracion de Firebase

---
+++ FirebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4Dqv6NMWScbqho8F7PYNjH-u0egGYpWA",
+  authDomain: "app-rutas-fb36d.firebaseapp.com",
+  projectId: "app-rutas-fb36d",
+  storageBucket: "app-rutas-fb36d.firebasestorage.app",
+  messagingSenderId: "170674374323",
+  appId: "1:170674374323:web:2ed297469f90544c3192c6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
